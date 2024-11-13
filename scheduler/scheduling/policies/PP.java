041b0dd25f6e6570a78e31fc23201f3f6445e482
AdrielArgueta <158535336+AdrielArgueta@users.noreply.github.com>
2024-11-13 00:41:00 -0600

    prueba clase

--- scheduler/scheduling/policies/PP.java
+++ scheduler/scheduling/policies/PP.java
@@ -1,4 +1,14 @@
+/*PP.java */
+/**
+** Hecho por: Adriel Levi Argueta Caal
+**Carnet: 24003171
+**Seccion: BN
+**/
+/*Descripcion */
 package scheduler.scheduling.policies;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
+import scheduler.processing.SimpleProcess;
 
 public class PP {
     
